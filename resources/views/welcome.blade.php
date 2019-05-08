44f49237a83fdc93bdb7bf51ffb97d90e6868ffd
cutelittlekitten <1033727@mborijnland.nl>
2019-05-08 13:58:54 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LVils
                 </div>
 
                 <div class="links">
